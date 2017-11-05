a6069c82898fa871ce4a06d9209e98818eba3a9c
pluthong <thongpaul@gmail.com>
2017-11-04 22:48:06 -0500

    implemented method add and remove player for SkunkApp

--- src/SkunkApp.java
+++ src/SkunkApp.java
@@ -1,5 +1,44 @@
+import java.util.ArrayList;
 
 public class SkunkApp {
+
+	private ArrayList<Player> players;
+	
+    public SkunkApp() {
+    	this.players = new ArrayList<>();
+    }
+    
+    // method returns true if player added to game
+ 	// otherwise false 
+ 	public boolean addPlayer(Player p)
+ 	{
+ 		// if player is already registered
+ 		if(this.players.contains(p))
+ 			return false;
+ 		// otherwise add player to game 
+ 		this.players.add(p);
+ 		return true;
+ 	}
+ 	
+ 	
+    // method returns true if player successfully removed
+  	// false otherwise 
+	public boolean removePlayer(Player p)
+	{
+		// if player is not in the game
+		if(!(this.players.contains(p)))
+			return false;
+		// otherwise remove player from the game
+		this.players.remove(p);
+		return true;
+	}
+	
+	
+		// get Name
+		public int getNumberOfPlayer() {
+			return this.players.size();
+		}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- test/SkunkAppTest.java
+++ test/SkunkAppTest.java
@@ -1,12 +1,33 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class SkunkAppTest {
+	
+	private SkunkApp app;
+	private Player p1;
+	private Player p2;
 
+	@Before
+	public void create_sample_player() {
+		app = new SkunkApp();
+		p2 = new Player("Eric");
+		p1 = new Player("Paul");
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_add_player_in_skunk() {
+		app.addPlayer(p1);
+		app.addPlayer(p2);
+		assertEquals("2 players added", 2, app.getNumberOfPlayer());
+	}
+	
+	@Test
+	public void test_remove_player_in_skunk() {
+		app.addPlayer(p1);
+		app.removePlayer(p1);
+		assertEquals("1 player removed", 0, app.getNumberOfPlayer());
 	}
 
 }
